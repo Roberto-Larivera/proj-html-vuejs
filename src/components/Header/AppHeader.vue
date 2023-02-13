a4725ff9bf4ada0d4214a88551db54d150ba8cd1
Roberto Larivera <robylariv@gmail.com>
2023-02-13 15:03:22 +0100

    Inizio progettazione header

--- src/components/Header/AppHeader.vue
+++ src/components/Header/AppHeader.vue
@@ -1,20 +1,48 @@
 <script>
 export default {
   name: 'AppHeader',
-  data(){
-    return{
+  data() {
+    return {
 
     }
   }
-  
+
 }
 </script>
 
 <template>
   <header>
-    AppHeader
+    <!-- BG SLIDERS HEADER  -->
+    <div class="bg-sliders">
+      <div class="container">
+        <!-- ROW TOP LINKS  -->
+        <div class="row">
+          <div class="col">
+            logo
+          </div>
+          <div class="col d-flex justify-content-end">
+            links
+          </div>
+        </div>
+        <!-- ROW BOTTOM JUMBOTRON  -->
+        <div class="row">
+          <div class="col-1">
+            freccia 1
+          </div>
+          <div class="col">
+            cards
+          </div>
+          <div class="col-1">
+            freccia 2
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- END - BG SLIDERS HEADER  -->
+
   </header>
 </template>
 
 <style lang="scss" scoped>
+
 </style>
