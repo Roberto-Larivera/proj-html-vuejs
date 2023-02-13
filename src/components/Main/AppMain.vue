4ec049aa12339533362c087e3ecfbec2c6634506
Roberto Larivera <robylariv@gmail.com>
2023-02-13 20:20:27 +0100

    Inizio AppMain + Creazione MainSectionSkill

--- src/components/Main/AppMain.vue
+++ src/components/Main/AppMain.vue
@@ -1,20 +1,29 @@
 <script>
+import MainSectionSkill from './MainSectionSkill.vue'
 export default {
   name: 'AppMain',
-  data(){
-    return{
+  components: {
+    MainSectionSkill,
+  },
+  data() {
+    return {
 
     }
   }
-  
+
 }
 </script>
 
 <template>
   <main>
-    AppMain
+    <section class="section_skill">
+      <MainSectionSkill />
+    </section>
   </main>
 </template>
 
 <style lang="scss" scoped>
+main {
+  background-color: $background_color-5;
+}
 </style>
---
+++ src/components/Main/MainSectionSkill.vue
@@ -0,0 +1,39 @@
+<script>
+import SectionTitle from '../All/SectionTitle.vue';
+export default {
+  name: 'MainSectionSkill',
+  components:{
+    SectionTitle,
+  },
+  data() {
+    return {
+
+    }
+  }
+
+}
+</script>
+
+<template>
+  <div>
+    <div class="container">
+      <div class="box_bg">
+        <div class="row">
+          <div class="col ">
+            <SectionTitle  :section="'h1'"/>
+            <SectionTitle  :section="'h2'" />
+            <SectionTitle  :section="'h3'"/>
+            <SectionTitle  :section="'h2'" :sectionAlign="'center'" />
+            <SectionTitle  :section="'h3'" :sectionAlign="'center'"/>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.box_bg{
+  background-color: $background_color-1;
+}
+</style>
