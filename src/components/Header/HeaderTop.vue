e0b1927481f82c5542d96b02ad4eed4b3b8e12a2
Roberto Larivera <robylariv@gmail.com>
2023-02-13 16:23:31 +0100

    mod HeaderTop links

--- src/components/Header/HeaderTop.vue
+++ src/components/Header/HeaderTop.vue
@@ -3,7 +3,32 @@ export default {
     name: 'HeaderTop',
     data() {
         return {
-
+            headerNavBarLink: [
+                {
+                    name: 'homes',
+                    href: '#nogo',
+                },
+                {
+                    name: 'pages',
+                    href: '#nogo',
+                },
+                {
+                    name: 'blog',
+                    href: '#nogo',
+                },
+                {
+                    name: 'shop',
+                    href: '#nogo',
+                },
+                {
+                    name: 'events',
+                    href: '#nogo',
+                },
+                {
+                    name: 'elements',
+                    href: '#nogo',
+                },
+            ]
         }
     }
 
@@ -17,45 +42,22 @@ export default {
         </div>
         <div class="col d-flex justify-content-end align-items-center">
             <ul class="d-flex justify-content-between align-content-center mb-0 w-100">
-                <li>
-                    <a href="#nogo">
-                        link                    
-                </a>
+               <template v-for="element in headerNavBarLink">
+                <li class="align-self-center">
+                    <a :href="element.href">
+                        {{ element.name }}
+                    </a>
                 </li>
-                <li>
-                    <a href="#nogo">
-                        link                    
-                </a>
+               </template>
+               <li class="align-self-center">
+                    <button>
+                        cerca
+                    </button>
                 </li>
-                <li>
-                    <a href="#nogo">
-                        link                    
-                </a>
-                </li>
-                <li>
-                    <a href="#nogo">
-                        link                    
-                </a>
-                </li>
-                <li>
-                    <a href="#nogo">
-                        link                    
-                </a>
-                </li>
-                <li>
-                    <a href="#nogo">
-                        link                    
-                </a>
-                </li>
-                <li>
-                    <a href="#nogo">
-                        button                    
-                </a>
-                </li>
-                <li>
-                    <a href="#nogo">
-                        button                    
-                </a>
+                <li class="align-self-center">
+                    <button>
+                        menu
+                    </button>
                 </li>
             </ul>
         </div>
@@ -63,11 +65,12 @@ export default {
 </template>
 
 <style lang="scss" scoped>
-.row{
-    ul{
-        li{
+.row {
+    ul {
+        li {
             list-style: none;
-            a{
+
+            a {
                 text-decoration: none;
             }
         }
