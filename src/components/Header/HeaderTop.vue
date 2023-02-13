1e0675dab48ee691697c0e468e624ca6437fd135
Roberto Larivera <robylariv@gmail.com>
2023-02-13 18:58:42 +0100

    mod HeaderTop hover links

--- src/components/Header/HeaderTop.vue
+++ src/components/Header/HeaderTop.vue
@@ -36,20 +36,23 @@ export default {
 </script>
 
 <template>
-    <div class="row row-cols-2 h-100 p-3 align-content-center">
-        <div class="col h-100 d-flex align-items-center">
+    <div class="row h-100 p-3 align-content-center justify-content-between">
+        <div class="col-auto h-100 d-flex align-items-center">
             <img class="h-25" src="../../assets/img/logo-img-01.png" alt="logo_Everlead">
         </div>
-        <div class="col d-flex justify-content-end align-items-center">
+        <div class="col-auto d-flex justify-content-end align-items-center">
             <ul class="d-flex justify-content-between align-content-center mb-0 w-100">
-               <template v-for="element in headerNavBarLink">
+                <template v-for="element in headerNavBarLink">
+                    <li class="list_link align-self-center p-2">
+                        <a :href="element.href">
+                            <font-awesome-icon icon="fa-solid fa-arrow-right-long" />
+                            <span>
+                                {{ element.name }}
+                            </span>
+                        </a>
+                    </li>
+                </template>
                 <li class="align-self-center">
-                    <a :href="element.href">
-                        {{ element.name }}
-                    </a>
-                </li>
-               </template>
-               <li class="align-self-center">
                     <button class="btn">
                         cerca
                     </button>
@@ -67,12 +70,32 @@ export default {
 <style lang="scss" scoped>
 .row {
     ul {
+
         li {
             list-style: none;
+
+            &:hover a {
+                color: $text_color-8;
+
+                .fa-arrow-right-long {
+                    display: inline-block;
+
+                }
+            }
+
+            &.list_link {
+
+                .fa-arrow-right-long {
+                    margin-right: 10px;
+                    display: none;
+                }
+            }
 
             a {
                 text-decoration: none;
                 text-transform: uppercase;
+                color: $text_color-14;
+                font-size: .9rem;
             }
         }
     }
