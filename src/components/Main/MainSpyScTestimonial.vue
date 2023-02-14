bec112a2e78af1a54027c5df567d466d46b792b1
Roberto Larivera <robylariv@gmail.com>
2023-02-14 21:47:26 +0100

    mod MainSectionTestimonials + aggiunta MainSpyScTestimonial

---
+++ src/components/Main/MainSpyScTestimonial.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  name: 'MainSpyScTestimonial',
+  data() {
+    return {
+
+    }
+  },
+  props: {
+    
+  }
+
+}
+</script>
+
+<template>
+  <div>
+    MainSpyScTestimonial
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
